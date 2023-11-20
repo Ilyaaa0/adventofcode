e48a42cf00cbdee71c0588b019a6842a3e8fb9a9
Ilyaaa0 <iliasuhodolets@gmail.com>
2023-11-20 17:46:58 +0300

    fix: remove useless unwrap

--- 22/first_day/src/main.rs
+++ 22/first_day/src/main.rs
@@ -8,7 +8,7 @@ fn main() -> io::Result<()> {
     let mut solutions: (i32, i32) = (0, 0);
     let mut raw_content = String::new();
 
-    reader.read_to_string(&mut raw_content).unwrap();
+    reader.read_to_string(&mut raw_content)?;
 
     let mut vec_sum: Vec<i32> = Vec::new();
 

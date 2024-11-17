03dcb89fb3ad26ce7a166de9f8468dee7287a714
Suhodolets Ilya <iliasuhodolets@gmail.com>
2024-11-17 12:59:45 +0300

    day4 solutions

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,13 +1,43 @@
+use md5;
 use std::{fs, io};
 
 fn main() -> io::Result<()> {
-    println!("day1: {}", day1(&fs::read_to_string("./src/input.txt")?));
+    let input = fs::read_to_string("./src/input.txt")?;
+
+    println!("{}", first_day(&input));
+    println!("{}", second_day(&input));
 
     Ok(())
 }
 
-fn day1(input: &str) -> i32 {
-    i32::MAX
+fn first_day(input: &str) -> i32 {
+    let mut result_hash = String::new();
+    let mut result = 0;
+    let input = input.trim();
+
+    while !result_hash.starts_with("00000") {
+        result += 1;
+
+        let hash = format!("{input}{result}");
+        result_hash = format!("{:?}", md5::compute(hash));
+    }
+
+    result
+}
+
+fn second_day(input: &str) -> i32 {
+    let mut result_hash = String::new();
+    let mut result = 0;
+    let input = input.trim();
+
+    while !result_hash.starts_with("000000") {
+        result += 1;
+
+        let hash = format!("{input}{result}");
+        result_hash = format!("{:?}", md5::compute(hash));
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -15,7 +45,14 @@ mod tests {
     use crate::*;
 
     #[test]
-    fn test_day1() {
-        assert!(true);
+    fn test_first_day_case1() {
+        let input = "abcdef";
+        assert_eq!(first_day(input), 609043);
+    }
+
+    #[test]
+    fn test_second_day_case2() {
+        let input = "pqrstuv";
+        assert_eq!(first_day(input), 1048970);
     }
 }

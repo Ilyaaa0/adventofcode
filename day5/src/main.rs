8373b32a8ec9ae6150bbf4110a0be5c19cdd175c
Suhodolets Ilya <iliasuhodolets@gmail.com>
2024-11-25 00:00:15 +0300

    add day5 solution (first part)

---
+++ day5/src/main.rs
@@ -0,0 +1,81 @@
+use std::{collections::HashSet, fs, io};
+
+fn main() -> io::Result<()> {
+    let input = fs::read_to_string("./src/input.txt")?;
+
+    println!("{}", first_day(&input));
+
+    Ok(())
+}
+
+fn first_day(input: &str) -> i32 {
+    let mut result = 0;
+    let input = input.trim();
+    let vowels = HashSet::from(['a', 'e', 'i', 'o', 'u']);
+
+    for line in input.lines() {
+        let list = line.chars().collect::<Vec<char>>();
+
+        let double_char_vec = list
+            .iter()
+            .enumerate()
+            .filter(|(i, _)| {
+                list.get(*i)
+                    .is_some_and(|x| list.get(i + 1).is_some_and(|y| x.eq(y)))
+            })
+            .collect::<Vec<_>>();
+
+        let vowels_count = list
+            .iter()
+            .filter_map(|x| vowels.get(&x))
+            .collect::<Vec<_>>();
+
+        if vowels_count.len() >= 3
+            && double_char_vec.len() != 0
+            && (line.contains("ab")
+                || line.contains("cd")
+                || line.contains("pq")
+                || line.contains("xy"))
+                == false
+        {
+            result += 1;
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn test_first_day_first_case() {
+        let input = "ugknbfddgicrmopn";
+        assert_eq!(first_day(&input), 1);
+    }
+
+    #[test]
+    fn test_first_day_second_case() {
+        let input = "aaa";
+        assert_eq!(first_day(&input), 1);
+    }
+
+    #[test]
+    fn test_first_day_third_case() {
+        let input = "haegwjzuvuyypxyu";
+        assert_eq!(first_day(&input), 0);
+    }
+
+    #[test]
+    fn test_first_day_fourth_case() {
+        let input = "haegwjzuvuyypxyu";
+        assert_eq!(first_day(&input), 0)
+    }
+
+    #[test]
+    fn test_first_day_fifth_case() {
+        let input = "dvszwmarrgswjxmb";
+        assert_eq!(first_day(&input), 0);
+    }
+}
